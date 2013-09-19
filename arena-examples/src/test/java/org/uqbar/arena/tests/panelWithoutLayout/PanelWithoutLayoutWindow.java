f902ad30e89450a81d17ceef89941f2546481305
Javier Fernandes <javier.fernandes@gmail.com>
2013-09-19 15:40:07 -0300

    adding GroupPanel

--- arena-examples/src/test/java/org/uqbar/arena/tests/panelWithoutLayout/PanelWithoutLayoutWindow.java
+++ arena-examples/src/test/java/org/uqbar/arena/tests/panelWithoutLayout/PanelWithoutLayoutWindow.java
@@ -1,6 +1,8 @@
 package org.uqbar.arena.tests.panelWithoutLayout;
 
+import org.uqbar.arena.layout.HorizontalLayout;
 import org.uqbar.arena.widgets.Button;
+import org.uqbar.arena.widgets.GroupPanel;
 import org.uqbar.arena.widgets.Panel;
 import org.uqbar.arena.windows.MainWindow;
 
@@ -14,8 +16,10 @@ public class PanelWithoutLayoutWindow extends MainWindow<PanelWithoutLayoutDomai
 
 	@Override
 	public void createContents(Panel mainPanel) {
-		Panel innerPanel = new Panel(mainPanel);
-		innerPanel.setLayout(null);
+		GroupPanel innerPanel = new GroupPanel(mainPanel);
+		innerPanel.setTitle("Buttons");
+		
+		innerPanel.setLayout(new HorizontalLayout());
 		Button b = new Button(innerPanel);
 		Button b2 = new Button(innerPanel);
 
