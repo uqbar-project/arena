44dea8ebff59947e37ebc2f8f03d2ccafe4375ec
nnydjesus <none@none>
2014-08-15 00:39:04 -0300

    observeEnabled para un Text en realidad se convierte en observeEditable. Esto estaba hecho en JFaceControlBuilder, pero cuando se creaban los bindings se estaba usando el trait JFaceEnabledDisabled

--- arena-jface/src/main/scala/org/uqbar/lacar/ui/impl/jface/builder/traits/JFaceEnabledDisabled.scala
+++ arena-jface/src/main/scala/org/uqbar/lacar/ui/impl/jface/builder/traits/JFaceEnabledDisabled.scala
@@ -2,9 +2,10 @@ package org.uqbar.lacar.ui.impl.jface.builder.traits
 
 import org.uqbar.lacar.ui.model.builder.traits.DisableEnable
 import org.uqbar.lacar.ui.impl.jface.bindings.JFaceBindingBuilder
-import org.eclipse.jface.databinding.swt.SWTObservables
+import org.eclipse.jface.databinding.swt.SWTObservables.{ observeVisible => observeVis, observeEditable, observeEnabled => observeEnab }
 import org.uqbar.lacar.ui.impl.jface.builder.JFaceWidgetBuilder
 import org.eclipse.swt.widgets.Control
+import org.eclipse.swt.widgets.Text
 
 /**
  * jface implementation of the trait disabled/enabled.
@@ -12,6 +13,6 @@ import org.eclipse.swt.widgets.Control
 trait JFaceEnabledDisabled extends DisableEnable {
   this : JFaceWidgetBuilder[_ <: Control] =>
   
-  override def observeEnabled() = new JFaceBindingBuilder(this, SWTObservables.observeEnabled(widget))
+  override def observeEnabled() = new JFaceBindingBuilder(this, if (widget.isInstanceOf[Text]) observeEditable(widget) else observeEnab(widget))
 
 }
