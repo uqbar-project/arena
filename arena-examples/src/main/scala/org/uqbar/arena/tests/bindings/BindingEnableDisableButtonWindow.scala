ae882c7b646cc122f62dea05bdf8e954519da028
Nicolás Passerini <npasserini@gmail.com>
2014-10-04 20:48:50 -0300

    Observing errors is now a binding also in Arena side.

--- arena-examples/src/main/scala/org/uqbar/arena/tests/bindings/BindingEnableDisableButtonWindow.scala
+++ arena-examples/src/main/scala/org/uqbar/arena/tests/bindings/BindingEnableDisableButtonWindow.scala
@@ -13,51 +13,45 @@ import org.uqbar.arena.layout.ColumnLayout
 import org.uqbar.arena.widgets.Label
 
 /**
- * Checking if bindEnable and disableOnError (BUTTON) interfeer each other 
+ * Checking if bindEnable and disableOnError (BUTTON) interfeer each other
  */
-object BindingEnableDisableButtonWindow extends MainWindow[Friend] (new Friend()) with App{
-	startApplication()
-	
-	override def createContents(mainPanel:Panel) {
-	  new ErrorsPanel(mainPanel, "Name: any except error - Lucky: numbers except 13")
-	  
-	  var panel = new Panel(mainPanel)
-	  panel.setLayout(new ColumnLayout(2))
-	  
-	  new Label(panel).setText("Name:")
-	  new TextBox(panel).value().bindToProperty("name")
-	  
-	  new Label(panel).setText("Lucky:")
-	  new TextBox(panel).value().bindToProperty("luckyNumber")
-	  
-	  val bindEnableDisable = new Button(panel).setCaption("Enable -> Disable")
-	  bindEnableDisable.bindEnabled(new NotNullObservable("name"))
-	  bindEnableDisable.disableOnError()
-	  
-      val bindDisableEnable = new Button(panel).setCaption("Disable -> Enable")
-      bindDisableEnable.disableOnError()
-	  bindDisableEnable.bindEnabled(new NotNullObservable("name"))
-	}
-  
+object BindingEnableDisableButtonWindow extends MainWindow[Friend](new Friend()) with App {
+  startApplication()
+
+  override def createContents(mainPanel: Panel) {
+    new ErrorsPanel(mainPanel, "Name: any except error - Lucky: numbers except 13")
+
+    var panel = new Panel(mainPanel)
+    panel.setLayout(new ColumnLayout(2))
+
+    new Label(panel).setText("Name:")
+    new TextBox(panel).value().bindToProperty("name")
+
+    new Label(panel).setText("Lucky:")
+    new TextBox(panel).value().bindToProperty("luckyNumber")
+
+    val bindEnableDisable = new Button(panel).setCaption("Enable -> Disable")
+    bindEnableDisable.bindEnabledToProperty("lucky")
+    bindEnableDisable.disableOnError()
+  }
 }
 
 @Observable
-class Friend {  
-  var name:String=null
-  var luckyNumber:Integer=24
+class Friend {
+  var name: String = null
+  var luckyNumber: Integer = 24
+  var lucky = true
   
-  def setName(theString:String)={
-    if(theString.equals("error")){
+  def setName(theString: String) = {
+    if (theString.equals("error")) {
       throw new UserException("Error is allways wrong")
     }
     name = theString
   }
-  
-  def setLuckyNumber(luckyOne:Integer) = {
-    if(luckyOne == 13){
-      throw new UserException("13 is never lucky")
-    }
-    luckyNumber = luckyOne 
+
+  def setLuckyNumber(luckyOne: Integer) = {
+    luckyNumber = luckyOne
+    lucky = luckyNumber != 13
   }
 
 }
