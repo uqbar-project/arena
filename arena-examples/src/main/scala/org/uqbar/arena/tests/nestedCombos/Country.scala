c7c7d111d3718f72319e5db87f46f15cde564555
nnydjesus <none@none>
2014-10-04 14:38:57 -0300

    Remove @Transactional from Country model.

--- arena-examples/src/main/scala/org/uqbar/arena/tests/nestedCombos/Country.scala
+++ arena-examples/src/main/scala/org/uqbar/arena/tests/nestedCombos/Country.scala
@@ -4,17 +4,13 @@ import org.uqbar.commons.utils.TransactionalAndObservable
 import scala.collection.mutable.Buffer
 import scala.collection.JavaConversions._
 import scala.collection.JavaConverters._
+import org.uqbar.commons.utils.Observable
 
-@TransactionalAndObservable
+@Observable
 class Country(var name: String) {
   var provinceName = ""
   var provinces: java.util.List[Province] = Buffer[Province]()
   var provinceSelected:Province=_
-  
-//  def this(a:String){
-//    this()
-    //provinces = Buffer[Province]()
-//  }
   
   def addProvince(p:String) = provinces.add(new Province(provinceName))
   def addProvince():Unit = provinces.add(new Province(provinceName))
