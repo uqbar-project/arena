f8a36f59a365d4ce047773edcfa09eb88a543de2
Gisela Decuzzi <giseladecuzzi@gmail.com>
2014-10-04 18:21:55 -0300

    better examples

--- arena-examples/src/main/scala/org/uqbar/arena/tests/bindings/BindingEnableDisableButtonWindow.scala
+++ arena-examples/src/main/scala/org/uqbar/arena/tests/bindings/BindingEnableDisableButtonWindow.scala
@@ -7,6 +7,10 @@ import org.uqbar.arena.bindings.NotNullObservable
 import org.uqbar.commons.utils.Observable
 import org.uqbar.arena.widgets.TextBox
 import org.uqbar.commons.model.UserException
+import org.uqbar.arena.windows.ErrorsPanel
+import org.uqbar.arena.windows.ErrorsPanel
+import org.uqbar.arena.layout.ColumnLayout
+import org.uqbar.arena.widgets.Label
 
 /**
  * Checking if bindEnable and disableOnError (BUTTON) interfeer each other 
@@ -15,15 +19,22 @@ object BindingEnableDisableButtonWindow extends MainWindow[Friend] (new Friend()
 	startApplication()
 	
 	override def createContents(mainPanel:Panel) {
-	  new TextBox(mainPanel).value().bindToProperty("name")
+	  new ErrorsPanel(mainPanel, "Name: any except error - Lucky: numbers except 13")
 	  
-	  new TextBox(mainPanel).value().bindToProperty("luckyNumber")
+	  var panel = new Panel(mainPanel)
+	  panel.setLayout(new ColumnLayout(2))
 	  
-	  val bindEnableDisable = new Button(mainPanel).setCaption("Enable -> Disable")
+	  new Label(panel).setText("Name:")
+	  new TextBox(panel).value().bindToProperty("name")
+	  
+	  new Label(panel).setText("Lucky:")
+	  new TextBox(panel).value().bindToProperty("luckyNumber")
+	  
+	  val bindEnableDisable = new Button(panel).setCaption("Enable -> Disable")
 	  bindEnableDisable.bindEnabled(new NotNullObservable("name"))
 	  bindEnableDisable.disableOnError()
 	  
-      val bindDisableEnable = new Button(mainPanel).setCaption("Disable -> Enable")
+      val bindDisableEnable = new Button(panel).setCaption("Disable -> Enable")
       bindDisableEnable.disableOnError()
 	  bindDisableEnable.bindEnabled(new NotNullObservable("name"))
 	}
@@ -46,7 +57,7 @@ class Friend {
     if(luckyOne == 13){
       throw new UserException("13 is never lucky")
     }
-    luckyNumber = luckyOne
+    luckyNumber = luckyOne 
   }
 
 }
