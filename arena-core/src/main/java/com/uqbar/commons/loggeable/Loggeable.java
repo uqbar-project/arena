8bb8c65d281e9c23ab58ef364a5e7638eb999a2a
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2014-11-11 10:57:01 -0300

    Adding Hierarchial Logger from commons-exceptions

---
+++ arena-core/src/main/java/com/uqbar/commons/loggeable/Loggeable.java
@@ -0,0 +1,16 @@
+package com.uqbar.commons.loggeable;
+
+import java.io.Serializable;
+
+/**
+ * DOCME:
+ * @author npasserini
+ */
+public interface Loggeable extends Serializable {
+	
+	/**
+	 * Se logea a traves de un objeto Logger quien visita a los Loggeables atraves de este metodo.
+	 * Ellos sabran agregarse al logger. 
+	 */
+	public void appendYourselfTo(HierarchicalLogger visitor);
+}
