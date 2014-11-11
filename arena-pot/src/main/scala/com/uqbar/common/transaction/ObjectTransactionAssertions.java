797af34680cac9217346bc335da039d79ab65424
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2014-11-11 11:19:12 -0300

    Renaming AssertUtils->ObjectTransactionAssertions
    Since it is POT specific. It is not a general utility

---
+++ arena-pot/src/main/scala/com/uqbar/common/transaction/ObjectTransactionAssertions.java
@@ -0,0 +1,28 @@
+package com.uqbar.common.transaction;
+
+
+/**
+ * @author jfernandes
+ * @since 3.5
+ */
+public class ObjectTransactionAssertions {
+
+	public static void assertNull(String message, Object obj) {
+		assertTrue(message, obj == null);
+	}
+
+	public static void assertNotNull(String message, Object obj) {
+		assertTrue(message, obj != null);
+	}
+
+	public static void assertTrue(String message, boolean condition) {
+		if (!condition) {
+			throw new ObjectTransactionException(message);
+		}
+	}
+
+	public static void assertArgumentNotNull(String message, Object argument) {
+		assertNotNull(message, argument);
+	}
+
+}
