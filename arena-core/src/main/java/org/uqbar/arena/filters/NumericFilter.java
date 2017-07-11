d5f1cb1521def76644e44485d227c896f5b388a5
fdodino <fernando.dodino@gmail.com>
2017-07-11 19:03:24 -0300

    Fix #35 & added some tests

--- arena-core/src/main/java/org/uqbar/arena/filters/NumericFilter.java
+++ arena-core/src/main/java/org/uqbar/arena/filters/NumericFilter.java
@@ -2,11 +2,24 @@ package org.uqbar.arena.filters;
 
 import org.uqbar.arena.widgets.TextInputEvent;
 
+/**
+ * Numeric Filter accepts
+ *  
+ * - positive
+ * - negative
+ * - decimal (comma and point)
+ * values
+ * 
+ */
 public class NumericFilter implements TextFilter {
 
 	@Override
 	public boolean accept(TextInputEvent event) {
-		return event.getPotentialTextResult().matches("[0-9,.]*");
+		return isNumeric(event.getPotentialTextResult());
 	}
 
+	public boolean isNumeric(String value) {
+		return value.matches("-?[0-9.]+(,[0-9])?");
+	}
+	
 }
